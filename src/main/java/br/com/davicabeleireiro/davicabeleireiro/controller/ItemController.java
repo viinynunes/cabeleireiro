d4cc0ee431c20f6b6c3c97c641e6eeef92071b4c
Vinicius Nunes <viiny_nunes@hotmail.com>
2021-07-25 22:44:08 -0300

    Item: Implemented Pagination on find All

--- src/main/java/br/com/davicabeleireiro/davicabeleireiro/controller/ItemController.java
+++ src/main/java/br/com/davicabeleireiro/davicabeleireiro/controller/ItemController.java
@@ -3,9 +3,14 @@ package br.com.davicabeleireiro.davicabeleireiro.controller;
 import br.com.davicabeleireiro.davicabeleireiro.model.dto.ItemDTO;
 import br.com.davicabeleireiro.davicabeleireiro.services.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/item")
@@ -14,13 +19,27 @@ public class ItemController {
     @Autowired
     private ItemService service;
 
+    @Autowired
+    private PagedResourcesAssembler<ItemDTO> assembler;
+
     @PostMapping(produces = "application/json", consumes = "application/json")
     public ItemDTO create(@RequestBody ItemDTO dto){
         return service.create(dto);
     }
 
     @GetMapping(produces = "application/json" ,consumes = "application/json")
-    public List<ItemDTO> findAll(){
-        return service.findAll();
+    public ResponseEntity<?> findAll(@RequestParam(value = "direction", defaultValue = "desc")String direction,
+                                     @RequestParam(value = "page", defaultValue = "0") int page,
+                                     @RequestParam(value = "size", defaultValue = "10") int size){
+
+        var sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "name"));
+
+        var itemPaged = service.findAll(pageable);
+
+        PagedModel<?> model = assembler.toModel(itemPaged);
+
+        return new ResponseEntity<>(model, HttpStatus.OK);
     }
 }
--- src/main/java/br/com/davicabeleireiro/davicabeleireiro/services/ItemService.java
+++ src/main/java/br/com/davicabeleireiro/davicabeleireiro/services/ItemService.java
@@ -1,15 +1,14 @@
 package br.com.davicabeleireiro.davicabeleireiro.services;
 
+import br.com.davicabeleireiro.davicabeleireiro.exception.ResourceNotFoundException;
 import br.com.davicabeleireiro.davicabeleireiro.model.dto.ItemDTO;
 import br.com.davicabeleireiro.davicabeleireiro.model.entities.Item;
 import br.com.davicabeleireiro.davicabeleireiro.repository.CategoryRepository;
 import br.com.davicabeleireiro.davicabeleireiro.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class ItemService {
@@ -21,15 +20,17 @@ public class ItemService {
     private CategoryRepository categoryRepository;
 
     public ItemDTO create(ItemDTO dto){
-        dto.setCategory(categoryRepository.findById(dto.getCategory().getId()).get());
+        dto.setCategory(categoryRepository.findById(dto.getCategory().getId()).orElseThrow(() -> new ResourceNotFoundException("Category "+ dto.getCategory().getId()+" not found")));
 
         return new ItemDTO(repository.save(new Item(dto)));
     }
 
-    public List<ItemDTO> findAll(){
-        var entityList = repository.findAll();
-        List<ItemDTO> dtoList = new ArrayList<>();
-        entityList.forEach(x -> dtoList.add(new ItemDTO(x)));
-        return dtoList;
+    public Page<ItemDTO> findAll(Pageable pageable){
+        var entityList = repository.findAll(pageable);
+        return entityList.map(this::convertToDTO);
+    }
+
+    private ItemDTO convertToDTO(Item item){
+        return new ItemDTO(item);
     }
 }
